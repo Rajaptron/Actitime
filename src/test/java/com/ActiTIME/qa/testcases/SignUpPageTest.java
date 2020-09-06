d94bfd5926646193b636e60e3ee80986309140b5
vineetece <vineet.ece2014@gmail.com>
2020-09-06 16:26:37 +0530

    Created Sign Up Page & Test Case for it.

---
+++ src/test/java/com/ActiTIME/qa/testcases/SignUpPageTest.java
@@ -0,0 +1,38 @@
+package com.ActiTIME.qa.testcases;
+
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.ActiTIME.qa.base.TestBase;
+import com.ActiTIME.qa.pages.SignUpPage;
+
+public class SignUpPageTest extends TestBase {
+
+	static SignUpPage sp;
+
+	public SignUpPageTest() {
+		super();
+		PageFactory.initElements(driver, this);
+	}
+
+	@BeforeMethod
+	public static void setup() throws InterruptedException {
+		initialization();
+		Thread.sleep(1000);
+		sp = new SignUpPage();
+	}
+
+	@Test
+	public static void SignUpFormTest() throws InterruptedException {
+		String Title = sp.SignUpForm("Vineet", "Kumar", "vineet.ece2015@gmail.com", "Test");
+		Assert.assertEquals(Title, "Free Timesheet Online Trial");
+	}
+
+	@Test
+	public static void ConfirmMessage() {
+
+	}
+
+}
